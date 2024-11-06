155c8fb591856ea4d60b3a0995d633550372755c
Sean Dooley <s.dooley@ctidigital.com>
2024-11-06 10:35:16 +0000

    introducing list your best friend

---
+++ WordScramble/TutorialView.swift
@@ -0,0 +1,49 @@
+//
+//  TutorialView.swift
+//  WordScramble
+//
+//  Created by Sean Dooley on 06/11/2024.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
+    var body: some View {
+        VStack {
+//            List(people, id: \.self) {
+//                Text($0)
+//            }
+            
+            List {
+                Section("Section 1") {
+                    Text("Static row 1")
+                    Text("Static row 2")
+                }
+                
+                Section("Section 2") {
+                    ForEach(0..<5) {
+                        Text("Dynamic row \($0)")
+                    }
+                }
+                
+                Section("Section 3") {
+                    Text("Static row 3")
+                    Text("Static row 4")
+                }
+                
+                Section("Section 4") {
+                    ForEach(people, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .listStyle(.grouped)
+        }
+    }
+}
+
+#Preview {
+    TutorialView()
+}
