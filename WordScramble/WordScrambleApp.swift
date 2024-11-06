93d913e2db404510ebedcc1404cebd633e604a94
Sean Dooley <s.dooley@ctidigital.com>
2024-11-06 10:30:19 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sean Dooley on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
